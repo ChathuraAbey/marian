55b87d1013ca4aae5b8c5d534303db0c0860c731
Marian Fernando <marian.fernando@ifsworld.com>
2022-09-23 17:27:42 +0530

    Commit Login Function

--- src/main/java/restassured/login.java
+++ src/main/java/restassured/login.java
@@ -1,10 +1,28 @@
 package restassured;
 
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class login {
 
     public void function()
     {
-        //method deficnition 
+        //method deficnition
 
     }
+    @Test
+    public void loginFunction()
+    {
+        String token =   given().formParam("grant_type", "password").
+                formParam("client_id", "IFS_aurena").
+                formParam("client_secret", "2Rpi5yNFwU8YzrOdeCi7").
+                formParam("scope", "openid").
+                formParam("username", "alain").
+                formParam("password", "alain").
+                when().post("https://ifscloud22r1bnt-cmb.rnd.ifsdevworld.com/auth/realms/ifscloud22r1bnt-cmb/protocol/openid-connect/token").
+                then().extract().response().jsonPath().getString("access_token");
+        System.out.println(token);
+    }
+
 }
